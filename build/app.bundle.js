194f74cd139830d58716b6eeb6c10c7e5cd662f1
joanne <joanneonis@gmail.com>
2019-02-21 08:41:31 +0100

    voice recognition v1

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -99,8 +99,119 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _babel_polyfill__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_polyfill__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _tensorflow__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./tensorflow */ "./js/tensorflow.js");
 /* harmony import */ var _tensorflow__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_tensorflow__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _artyom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./artyom */ "./js/artyom.js");
+/* harmony import */ var _artyom__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_artyom__WEBPACK_IMPORTED_MODULE_2__);
 
 
+
+
+/***/ }),
+
+/***/ "./js/artyom.js":
+/*!**********************!*\
+  !*** ./js/artyom.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var artyom = new Artyom();
+var buttonTrigger = document.getElementById("sayword");
+var startBtn = document.getElementById("start");
+var stopBtn = document.getElementById("stop");
+var speechLog = document.getElementById("speech-log__inner");
+var textSaid; // artyom.initialize({
+// 	continuous:true,
+// 	lang:"nl-NL",
+// 	executionKeyword: "and do it now",
+// 	listen:true,
+// 	debug:true
+// });
+
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+} catch (e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+/*-----------------------------
+      Voice Recognition 
+------------------------------*/
+// If false, the recording will stop after a few seconds of silence.
+// When true, the silence period is longer (about 15 seconds),
+// allowing us to keep recording even when the user pauses. 
+
+
+recognition.continuous = false; // This block is called every time the Speech APi captures a line. 
+
+recognition.onresult = function (event) {
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far. 
+  // We only need the current one.
+  var current = event.resultIndex; // Get a transcript of what was said.
+
+  var transcript = event.results[current][0].transcript; // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+
+  var mobileRepeatBug = current == 1 && transcript == event.results[0][0].transcript;
+
+  if (!mobileRepeatBug) {
+    textSaid += transcript;
+  }
+
+  console.log("Herkend:", event.results[current][0].transcript);
+  speechStuff.appendText(event.results[current][0].transcript);
+};
+
+recognition.onstart = function () {
+  console.log('started recording');
+};
+
+recognition.onspeechend = function () {
+  console.log('ended recording');
+};
+
+recognition.onerror = function (event) {
+  if (event.error == 'no-speech') {
+    console.log('No speech was detected. Try again.');
+  }
+
+  ;
+};
+
+var speechStuff = {
+  init: function init() {
+    document.addEventListener('DOMContentLoaded', function () {
+      buttonTrigger.addEventListener("click", function () {// this.sayWord('data eyecatcher kan nederlands praten');
+      });
+      startBtn.addEventListener("click", function () {
+        recognition.start();
+      });
+      stopBtn.addEventListener("click", function () {
+        recognition.stop();
+      });
+    });
+  },
+  // sayWord(word) {
+  // 	artyom.say(word,{
+  // 		onStart:function(){
+  // 				console.log("Start tekst voorlezen");
+  // 		},
+  // 		onEnd:function(){
+  // 				console.log("Tekst gelezen!");
+  // 		}
+  // 	});
+  // }
+  appendText: function appendText(text) {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(text);
+    node.appendChild(textnode);
+    speechLog.appendChild(node);
+  }
+};
+speechStuff.init();
 
 /***/ }),
 
