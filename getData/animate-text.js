ad86a8e06c7cd800fe52faedbde8bd16f1413b1f
joanne <joanneonis@gmail.com>
2019-03-19 18:22:57 +0100

    slechte test

--- getData/animate-text.js
+++ getData/animate-text.js
@@ -21,3 +21,41 @@
 // 	// cancelAnimationFrame(request) 
 // 	requestAnimationFrame(performAnimation);
 // });
+
+let indexLetter = 0;
+let indexWord = 1;
+
+document.addEventListener("DOMContentLoaded", () => {
+	const textPath = document.querySelector('#textPath');
+	const textContent = document.querySelector('#textContent');
+	
+	window.setInterval(function(){
+		// var textnode = document.createTextNode(`${loremArray[indexWord]} `);
+		// textContent.prepend(textnode);
+
+		// textContent.prepend(`${loremArray[indexWord]} `);
+
+		console.log(loremArray[1].length);
+
+		const letters = loremArray[indexWord].split('');
+
+		for(let i = 0; i < loremArray[indexWord].length; i++) {
+			setTimeout(function(){
+				textContent.append(letters[i])
+			}, 500);
+		}
+
+		textContent.append(' ')
+		indexWord ++;
+	}, 2000);
+});
+
+
+
+
+
+// const letters = loremArray[indexWord].split('');
+
+// letters.forEach(letter => {
+// 	textContent.append(letter); 
+// });
