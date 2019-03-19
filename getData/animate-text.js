797be1fdb7b3374b56ed0951ea1b0939242d0204
joanne <joanneonis@gmail.com>
2019-03-19 17:31:54 +0100

    test with animate tag and test with requestanimationframe

---
+++ getData/animate-text.js
@@ -0,0 +1,23 @@
+
+// let request;
+// let frame = 0;
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+// 	const textPath = document.querySelector('#textPath');
+// 	const textContent = document.querySelector('#textContent');
+
+// 	console.log('ready');
+	
+// 	textPath.setAttribute("startOffset", `${1}%`);
+
+	
+	
+// 	const performAnimation = () => {
+// 		frame += .05;
+// 		request = requestAnimationFrame(performAnimation)
+// 		// textPath.setAttribute("startOffset", `${frame}%`);
+// 	}
+// 	// cancelAnimationFrame(request) 
+// 	requestAnimationFrame(performAnimation);
+// });
