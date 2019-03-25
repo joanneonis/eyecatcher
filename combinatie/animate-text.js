762a7298bee5f52d9f1d2efdbab17d899e912358
joanne <joanneonis@gmail.com>
2019-03-25 17:49:22 +0100

    start test 2

--- combinatie/animate-text.js
+++ combinatie/animate-text.js
@@ -5,28 +5,28 @@ document.addEventListener("DOMContentLoaded", () => {
 	const textPath = document.querySelector('#textPath');
 	const textContent = document.querySelector('#textContent');
 
-	// window.setInterval(function(){
-	// 	let letters = loremArray[indexWord].split('');
-	// 	letters.reverse();
+	window.setInterval(function(){
+		let letters = loremArray[indexWord].split('');
+		letters.reverse();
 
-	// 	if(textContent.textContent.length > 43) {  // 43 fit in tube
-	// 		//? if prepend last character should be removed & also letters.reverse() should be there
-	// 		textContent.textContent = textContent.textContent.substring(0, textContent.textContent.length-1);
-	// 	}
+		if(textContent.textContent.length > 43) {  // 43 fit in tube
+			//? if prepend last character should be removed & also letters.reverse() should be there
+			textContent.textContent = textContent.textContent.substring(0, textContent.textContent.length-1);
+		}
 
-	// 	if (i < letters.length) {
-	// 		textContent.prepend(letters[i]);
-	// 	} else {
-	// 		textContent.prepend(' ');
-	// 		indexWord ++;
-	// 		i = -1;
-	// 	}
-	// 	i++;
+		if (i < letters.length) {
+			textContent.prepend(letters[i]);
+		} else {
+			textContent.prepend(' ');
+			indexWord ++;
+			i = -1;
+		}
+		i++;
 
-	// 	if(textContent.textContent.length > 43) {  // 43 fit in tube, 
-	// 		//? if append first item should be removed
-	// 		// textContent.textContent = textContent.textContent.substr(1);
-	// 	}
-	// }, 200);
+		if(textContent.textContent.length > 43) {  // 43 fit in tube, 
+			//? if append first item should be removed
+			// textContent.textContent = textContent.textContent.substr(1);
+		}
+	}, 200);
 
 });
